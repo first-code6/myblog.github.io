d8cca5805e8512b0cb88667d4b03d6867a981047
Coder-Start <794184829@qq.com>
2024-09-19 20:44:44 +0800

    新添动画和背景泛光

--- src/pages/Home/component/DataList.jsx
+++ src/pages/Home/component/DataList.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import data from '../../../data'
+import '../style/DataList.css'
 
 const DataList = ({
   dataType="nullData"
 }) => {
+
+  const [pageStyle, setPageStyle] = useState({
+    marginTop: "20px",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    boxSizing: "border-box",
+    padding: "10px",
+    userSelect: "none",
+    position: "relative",
+    border: "1px solid rgb(100, 100, 100)",
+    cursor: "pointer",
+    transition: "margin-top 0.3s ease-in-out",                
+  })
+
   return (
     <div
       style={{ 
-        marginTop: "20px",
         width: "40%",
         minWidth: "280px",
         height: "100%",
@@ -18,28 +33,12 @@ const DataList = ({
         data.listData[dataType].map(item => {
           return(
             <div
-              style={{
-                width: "100%",
-                height: "10%",
-                boxSizing: "border-box",
-                padding: "10px",
-                userSelect: "none",
-                position: "relative",
-                border: "1px solid rgb(100, 100, 100)"
+              className="page"
+              onMouseEnter={(e) => {
+                console.log(e)
               }}
               key={item.header}
             >
-
-              <a
-                href=""
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                }} 
-              />
 
               <p
                 style={{
@@ -51,7 +50,10 @@ const DataList = ({
 
               <p style={{
                 marginTop: "10px",
-                fontSize: "0.8em"
+                fontSize: "0.8em",
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
               }}>
                 {item.info}
               </p>
@@ -59,8 +61,8 @@ const DataList = ({
               <p style={{
                 marginTop: "10px",
                 fontSize: "0.8em",
-                float: "right",
                 color: "rgb(180, 180, 180)",
+                alignSelf: "flex-end"
               }}>
                 {item.date}
               </p>
